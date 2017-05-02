98c899e4270f6887ba81a3e0946e2227220cfc73
stephaniehchen <stephaniechen@Stephanie-MacBook-Air.local>
2017-05-02 11:48:34 -0500

    Update 1
    Missing ability to update

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,51 @@
+class PhotosController < ApplicationController
+  def new_form
+    #nothing needed here since links to create_row
+    render("photos/new_form.html.erb")
+  end
+
+  def create_row
+    url = params[:url]
+    caption = params[:caption]
+    new_photo = Photo.new
+    new_photo.source = url
+    new_photo.caption = caption
+    new_photo.save
+    #render("photos/create_row.html.erb")
+    redirect_to("/photos")
+  end
+
+  def index
+    @list_of_all_my_photos = Photo.all
+    render("photos/index.html.erb")
+  end
+
+  def show
+    the_id = params[:id]
+    @my_photo = Photo.find(the_id)
+    render("photos/show.html.erb")
+  end
+
+  def edit_form
+    the_id = params[:id]
+    @my_photo = Photo.find(the_id)
+    render("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    the_id = params[:id]
+    url = params[:url]
+    caption = params[:caption]
+    update_photo = Photo.find(the_id)
+    update_photo.source = url
+    update_photo.caption = caption
+    update_photo.save
+    #render("photos/create_row.html.erb")
+    redirect_to("/photos/update_photo.id")
+    #render("photos/update_row.html.erb")
+  end
+
+  def destroy_row
+    render("photos/destroy_row.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #CREATE
+  get("/photos/new", {:controller => "photos", :action => "new_form"})
+  get("/photos/create_photo", {:controller => "photos", :action => "create_row"})
+  #READ
+  get("/photos", {:controller => "photos", :action => "index"})
+  get("/photos/:id", {:controller => "photos", :action => "show"})
+  #UPDATE
+  get("/photos/update_photo/:id", {:controller => "photos", :action => "update_row"})
+  get("/photos/:id/edit", {:controller => "photos", :action => "edit_form"})
+  get("/photos/delete_photo/:id", {:controller => "photos", :action => "destroy_row"})
+
 end
